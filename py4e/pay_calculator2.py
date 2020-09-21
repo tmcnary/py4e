4b22696c5093de414e12994d526f55475a10cc47
Tony McNary <Tony.McNary@panerabread.com>
2020-09-21 13:24:18 -0500

    Adding first function and adding function to pay calc

--- py4e/pay_calculator2.py
+++ py4e/pay_calculator2.py
@@ -1,16 +1,22 @@
 print("Welcome to Pay Calculator")
+def computepay(fhours, frate):
+    print("In computepay", fhours, frate)
+    if fhours > 40 :
+        print("You've earned overtime pay")
+        rpay = 40 * frate
+        otpay = (fhours - 40) * (frate * 1.5)
+        pay = rpay + otpay
+        print("Your regular pay is:",rpay, "Your overtime pay is:",otpay)
+    else:
+        print("No overtime was earned")
+        pay = fhours * frate
+    print("Returning",pay)
+    return pay
+
 shours = input("Enter your hours: ")
 srate = input("Enter your pay rate: ")
 fhours = float(shours)
 frate = float(srate)
-print(fhours, frate)
-if fhours > 40 :
-    print("You've earned overtime pay")
-    rpay = 40 * frate
-    otpay = (fhours - 40) * (frate * 1.5)
-    print("Your regular pay is:",rpay, "Your overtime pay is:",otpay)
-    pay = rpay + otpay
-else:
-    print("No overtime was earned")
-    pay = fhours * frate
-print("Your pay is:",pay)
+
+xp = computepay(fhours, frate)
+print("Your pay is:",xp)
